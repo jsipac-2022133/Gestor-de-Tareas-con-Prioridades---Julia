b4bd2c2402b418a035d64a3f7e3d2d36ab14e4d2
jsipac-2022133 <jsipac-2022133@kinal.edu.gt>
2026-05-07 21:44:06 -0600

    feat: logica de cola de pendientes

--- main.jl
+++ main.jl
@@ -4,7 +4,17 @@ queue = PendingQueue(nothing, nothing, 0)
 tree = PriorityTree(nothing, 0)
 completed = CompletedList(nothing, nothing, 0)
 
-println("Gestor de tareas iniciado")
-println("Cola: ", queue.size, " tareas")
-println("Arbol: ", tree.size, " tareas")
-println("Completadas: ", completed.size, " tareas")
+t1 = create_task(1, "Comprar insumos", 2)
+t2 = create_task(2, "Llamar al cliente", 1)
+t3 = create_task(3, "Enviar reporte", 3)
+
+enqueue(queue, t1)
+enqueue(queue, t2)
+enqueue(queue, t3)
+
+println("Tareas en cola: ", queue.size)
+println("Siguiente tarea: ", peek_queue(queue).title)
+
+primera = dequeue(queue)
+println("Desencolada: ", primera.title)
+println("Tareas en cola: ", queue.size)
--- structures.jl
+++ structures.jl
@@ -34,3 +34,39 @@ mutable struct CompletedList
     tail::Union{ListNode, Nothing}
     size::Int
 end
+
+function create_task(id::Int, title::String, priority::Int)
+    return TaskNode(id, title, priority, time(), nothing, nothing)
+end
+
+function enqueue(q::PendingQueue, task::TaskNode)
+    node = QueueNode(task, nothing)
+    if q.tail === nothing
+        q.head = node
+        q.tail = node
+    else
+        q.tail.next = node
+        q.tail = node
+    end
+    q.size += 1
+end
+
+function dequeue(q::PendingQueue)
+    if q.head === nothing
+        return nothing
+    end
+    task = q.head.task
+    q.head = q.head.next
+    if q.head === nothing
+        q.tail = nothing
+    end
+    q.size -= 1
+    return task
+end
+
+function peek_queue(q::PendingQueue)
+    if q.head === nothing
+        return nothing
+    end
+    return q.head.task
+end
